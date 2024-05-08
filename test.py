f4d2e66a4c01a105775f1693b783de8a5837e62a
lalchhabi <tmgchhabi555@gmail.com>
2024-05-08 21:44:51 +0545

    train and test file update

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 if __name__ == '__main__':
 # Load a model
-    model = YOLO('C:/Users/User/Desktop/YOLOV8/runs/obb/train44/weights/best.pt')  # load a custom model
+    model = YOLO('path of the trained model')  # load a custom model
     # Predict with the model
-    results = model('C:/Users/User/Desktop/Internship_Crimson/data/', save = True)  # predict on an image
+    results = model('path of the infer data', save = True)  # predict on an image
 
     
--- train.py
+++ train.py
@@ -2,8 +2,8 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     # Load a model
     # model = YOLO('yolov8n-obb.yaml')  # build a new model from YAML
-    model = YOLO('yolov8m-obb.pt')  # load a pretrained model (recommended for training)
+    model = YOLO('path of the model(yolov8m-obb.pt)')  # load a pretrained model (recommended for training)
     # model = YOLO('yolov8n-obb.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
     # Train the model
-    results = model.train(data='data_config.yaml', epochs=100, imgsz=640, batch = -1)
+    results = model.train(data='path of config file(data_config.yaml)', epochs=100, imgsz=640, batch = -1)
